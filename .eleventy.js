4bccca4cfb136a50d016e9373c8da1299e234125
Yannick <lanfyp@gmail.com>
2023-01-17 21:46:02 +0100

    modif chemin img

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function (eleventyConfig) {
     // Ce dossier est cloné lors du build
-    eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("../src/img/uploads");
+    eleventyConfig.addPassthroughCopy("../src/css");
+    eleventyConfig.addPassthroughCopy("../src/js");
+    
     return {
         dir: {
             input: "src",
