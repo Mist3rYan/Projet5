aed1172d87a80c80b21879793e2a46d74e100b05
Yannick <lanfyp@gmail.com>
2023-01-17 23:08:37 +0100

    correction erreur js contact

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
 module.exports = function (eleventyConfig) {
+    // Ajout de la collection "images"
     // Ce dossier est cloné lors du build
     eleventyConfig.addPassthroughCopy("../src/img/uploads");
     eleventyConfig.addPassthroughCopy("../src/css");
     eleventyConfig.addPassthroughCopy("../src/js");
+
+    const glob = require("glob");
+    eleventyConfig.addCollection("images", function(collection) {
+        const images = glob.sync("../sources/img/uploads/*");
+        console.log(images);
+        return images.map(image => {
+            return {
+                url: image,
+                name: path.basename(image)
+            };
+        });
+    });
     
     return {
         dir: {
