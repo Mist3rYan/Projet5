34de21e8a2c39f6e7fe2490754eb05e262f3e227
Yannick <lanfyp@gmail.com>
2023-01-17 17:57:24 +0100

    modification architecture

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+    // Ce dossier est cloné lors du build
+    eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
     return {
         dir: {
             input: "src",
