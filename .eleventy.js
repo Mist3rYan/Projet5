122af890ca4627e5b40f88caf2c5ee2c3df845cb
Yannick <lanfyp@gmail.com>
2023-01-17 23:19:27 +0100

    modif path js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (eleventyConfig) {
     // Ajout de la collection "images"
     // Ce dossier est cloné lors du build
-    eleventyConfig.addPassthroughCopy("../src/img/uploads");
-    eleventyConfig.addPassthroughCopy("../src/css");
-    eleventyConfig.addPassthroughCopy("../src/js");
+    eleventyConfig.addPassthroughCopy("./src/img/uploads");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
 
     const glob = require("glob");
     eleventyConfig.addCollection("images", function(collection) {
-        const images = glob.sync("../sources/img/uploads/*");
+        const images = glob.sync("./sources/img/uploads/*");
         console.log(images);
         return images.map(image => {
             return {
