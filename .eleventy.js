b6c00cc5626e7ac2740dbbc041a42a0854fec343
Yannick <lanfyp@gmail.com>
2023-01-18 18:21:53 +0100

    maj galerie

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,36 @@
+const Image = require("@11ty/eleventy-img");
+const glob = require("glob-promise");
+
 module.exports = function (eleventyConfig) {
     // Ajout de la collection "images"
     // Ce dossier est cloné lors du build
     eleventyConfig.addPassthroughCopy("./src/img/uploads");
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/js");
-
-    const glob = require("glob");
-    eleventyConfig.addCollection("images", function(collection) {
-        const images = glob.sync("../img/uploads/*");
-        console.log(images);
-        return images.map(image => {
-            return {
-                url: image,
-                name: path.basename(image)
-            };
-        });
-    });
     
+    eleventyConfig.addCollection('images', async collectionApi => {
+
+      let files = await glob('../public/img/images/*.jpeg');
+      //Now filter to non thumb-
+      let images = files.filter(f => {
+        return f.indexOf('../public/img/images/thumb-') !== 0;
+      });
+  
+      let collection = images.map(i => {
+        let alt = i.split('/').pop();
+        alt=alt.split('.')[0];
+        return {
+          path: i.replace('../public/img/images/', '/img/images/'),
+          thumbpath: i.replace('../public/img/images/', '/img/images/thumb-'),
+          name: i.split('/').pop(),
+          alt: alt
+        }
+      });
+  
+      return collection;
+  
+    });
+
     return {
         dir: {
             input: "src",
